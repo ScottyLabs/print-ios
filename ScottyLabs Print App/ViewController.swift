e5ddec52a2afdc980d85309259284402ed807a95
Josh Zhanson <jzhanson@Joshs-MacBook-Pro.local>
2017-01-02 11:02:05 -0800

    Initial Commit

---
+++ ScottyLabs Print App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScottyLabs Print App
+//
+//  Created by Josh Zhanson on 1/2/17.
+//  Copyright © 2017 ScottyLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
