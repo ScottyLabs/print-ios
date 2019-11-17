36d71c8c311bd8e5dc0c9708a13fec4f0781aea5
Srinivas Lade <srinulade1@gmail.com>
2019-11-16 19:10:23 -0500

    Updated gitignore with Pods and Certain Project User Files

--- Pods/Target Support Files/Pods-ScottyLabs Print App/Pods-ScottyLabs Print App-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_ScottyLabs_Print_AppVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ScottyLabs_Print_AppVersionString[];
-
