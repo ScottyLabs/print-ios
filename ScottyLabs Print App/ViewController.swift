d355839e2a6f15e57108458424574043f6f38f0e
Srinivas Lade <srinulade1@gmail.com>
2019-11-16 20:34:21 -0500

    Clean Code Formatting and Styling

--- ScottyLabs Print App/ViewController.swift
+++ ScottyLabs Print App/ViewController.swift
@@ -1,40 +1,26 @@
-//
-//  ViewController.swift
-//  ScottyLabs Print App
-//
-//  Created by Josh Zhanson on 1/2/17.
-//  Copyright © 2017 ScottyLabs. All rights reserved.
-//
-
 import UIKit
 import Alamofire
 import SwiftyJSON
 
+
 struct persistData {
-    static var andrewID = ""
-    static var savedEnteredText = ""
+  static var andrewID = ""
+  static var savedEnteredText = ""
 }
+
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  override func viewDidLoad() {
+      super.viewDidLoad()
+  }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+  }
     
-    //    override var preferredStatusBarStyle: UIStatusBarStyle {
-    //        return .lightContent
-    //    }
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    
+  override var prefersStatusBarHidden: Bool {
+      return true
+  }
     
 }
-
