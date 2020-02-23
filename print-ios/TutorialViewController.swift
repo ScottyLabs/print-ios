47da44653dd02e2434dd41826e6631af0d692840
Srinivas Lade <srinulade1@gmail.com>
2020-02-23 00:13:37 -0500

    Simplification of Main UI
    - Cleanup of Main View Controller
    - Simplify Setting Default Andrew ID
    - Settings Changes (Name, Light Status Bar)

--- print-ios/TutorialViewController.swift
+++ print-ios/TutorialViewController.swift
@@ -2,155 +2,26 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class TutorialViewController: UIViewController, UITextFieldDelegate {
+// Constants
+let andrewIDKey = "andrewID"
 
+class TutorialViewController: UIViewController {
+
+  @IBOutlet weak var andrewIDTextField: UITextField!
+
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    andrewIDTextField.delegate = self
-    let tapRecogniser = UITapGestureRecognizer()
-    tapRecogniser.addTarget(self, action: #selector(self.viewTapped))
-    self.view.addGestureRecognizer(tapRecogniser)
-  }
-    @objc func viewTapped(){
-       self.view.endEditing(true)
-    }
-
-  @IBAction func saveButtonClick(_ sender: Any) {
-    let defaults = UserDefaults.standard
-
-    defaults.set(displayAndrewID.text, forKey: "andrewID")
-    defaults.synchronize()
-    print("ID=\(String(describing: displayAndrewID.text))")
-  }
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-  }
-  @IBAction func clearButtonClick(_ sender: Any) {
-    if(displayAndrewID.text == ""){
-      loadDefaults()
-      clearButton.setTitle("Clear", for: UIControl.State.normal)
-    }
-    else {
-      displayAndrewID.text = ""
-      clearButton.setTitle("Load", for: UIControl.State.normal)
-    }
+    
+    // Load Andrew ID from UserDefaults
+    andrewIDTextField.text = UserDefaults.standard.object(forKey: andrewIDKey) as? String
   }
   
-  func loadDefaults() {
-    let defaults = UserDefaults.standard
-    displayAndrewID.text = (defaults.object(forKey: "andrewID") as! String)
-  }
-  
-  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == andrewIDTextField {
-           textField.resignFirstResponder()
-        }
-        return true
+  @IBAction func onAndrewIDChange() {
+    if let newAndrewID = andrewIDTextField.text {
+      print("Setting New Andrew ID: \(newAndrewID)")
+      UserDefaults.standard.set(newAndrewID, forKey: andrewIDKey)
     }
-    
-    @IBOutlet weak var andrewIDTextField: UITextField!
-    override var prefersStatusBarHidden: Bool {
-    return true
-  }
-    
-  @IBOutlet weak var clearButton: UIButton!
-  @IBOutlet weak var displayAndrewID: UILabel!
-  @IBAction func submitIDbuttonClick(_ sender: Any) {
-        viewTapped()
-        let name: String = andrewIDTextField.text!
-        displayAndrewID.text = " \(name)  "
-        persistData.andrewID = name
-    }
-   // @IBAction func shareButton(sender: UIBarButtonItem) {
-    //let bundle = Bundle.main
-   // let samplePath = bundle.url(forResource: "sample", withExtension: "pdf")
-    //displayShareSheet(shareContent: samplePath as Any)
-  
-  @IBOutlet weak var copiesNum: UILabel!
-  
-  @IBAction func copyStepper(_ sender: UIStepper) {
-    (copiesNum.text = String(sender.value))
-  }
-  @IBAction func printSampleButton(sender: UIButton) {
-    showAndrewIDAlert()
-  }
-    
-  func alamofireUpload() {
-    // NSBundle for getting the path for sample.pdf
-    let bundle = Bundle.main
-    let samplePath = bundle.path(forResource: "sample", ofType: "pdf")
-        
-    let parameters = [
-      "andrew_id" : persistData.andrewID,
-      "copies": "1",
-      "sides": "one-sided"
-    ]
-    
-    Alamofire.upload(multipartFormData: { multipartFormData in
-            
-      // I don't think the order matters for appending
-      for (key, value) in parameters {
-        multipartFormData.append(value.data(using: .utf8)!, withName: key)
-      }
-            
-      if let firstData = NSData(contentsOfFile: samplePath!) {
-        var bytes = [UInt8]()
-        var buffer = [UInt8](repeating: 0, count: firstData.length)
-        firstData.getBytes(&buffer, length: firstData.length)
-        bytes = buffer
-        let data : Data = Data(bytes)
-                
-        // withName has to be file, I think, for the POST API to receive it
-        multipartFormData.append(data, withName: "file", fileName: "sample.pdf", mimeType: "application/pdf")
-      }
-            
-    }, to: "http://apis.scottylabs.org/print/v0/printfile", encodingCompletion: { result in
-      switch result {
-      case .success(let upload, _, _):
-                
-        upload.responseJSON { response in
-          if let JSON = response.result.value {
-            if let status = response.response?.statusCode {
-              let dict = JSON as! NSDictionary
-              
-              switch status {
-              case 200:
-                fallthrough
-              default:
-                if let message = dict["message"] {
-                  if let statusCode = dict["status_code"] {
-                    self.showSampleAlert(info: (message as! String) + "\n Status code: \(statusCode)")
-                  }
-                }
-              }
-            }
-          }
-        }
-            
-      case .failure(let encodingError):
-        self.showSampleAlert(info: (encodingError) as! String + "\n Encoding failed!")
-      }
-    })
-  }
-    
-  func displayShareSheet(shareContent:Any) {
-    let activityViewController = UIActivityViewController(activityItems: [shareContent as! URL], applicationActivities: nil)
-    present(activityViewController, animated: true, completion: {})
-  }
-    
-  func showSampleAlert(info: String) {
-    let pushPrompt = UIAlertController(title: "Print sample", message: info, preferredStyle: .alert)
-    let ok = UIAlertAction(title: "OK", style: .default, handler: { (action: UIAlertAction!) in })
-    pushPrompt.addAction(ok)
-    present(pushPrompt, animated: true, completion: nil )
-  }
-    
-  func showAndrewIDAlert() {
-    let bundle = Bundle.main
-       let samplePath = bundle.url(forResource: "sample", withExtension: "pdf")
-       displayShareSheet(shareContent: samplePath as Any)
-
-      self.alamofireUpload()
   }
 
 }
