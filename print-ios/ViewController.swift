f691193146df13312169e15424efa920dad9b8b1
Richard G <rwguo@andrew.cmu.edu>
2020-05-19 18:30:41 -0500

    changed indentation to 4 spaces

--- print-ios/ViewController.swift
+++ print-ios/ViewController.swift
@@ -4,24 +4,24 @@ import UIKit
 let andrewIDKey = "andrewID"
 
 class ViewController: UIViewController {
-
-  @IBOutlet weak var andrewIDTextField: UITextField!
-
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-    // Load Andrew ID from UserDefaults
-    andrewIDTextField.text = UserDefaults.standard.object(forKey: andrewIDKey) as? String
-  }
-  
-  // TODO: It should be updated upon finish instead of change
-  // Also, it should have a "Save" button
-  @IBAction func onAndrewIDChange() {
-    if let newAndrewID = andrewIDTextField.text {
-      print("Setting New Andrew ID: \(newAndrewID)")
-      UserDefaults.standard.set(newAndrewID, forKey: andrewIDKey)
+    // Properties
+    @IBOutlet weak var andrewIDTextField: UITextField!
+    @IBOutlet weak var mapBtn: UIButton!
+    @IBOutlet weak var feedbackBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load Andrew ID from UserDefaults
+        andrewIDTextField.text = UserDefaults.standard.object(forKey: andrewIDKey) as? String
     }
-  }
-
+    
+    @IBAction func onAndrewIDEnd(_ sender: Any) {
+        if let newAndrewID = andrewIDTextField.text {
+            print("Setting New Andrew ID: \(newAndrewID)")
+            UserDefaults.standard.set(newAndrewID, forKey: andrewIDKey)
+        }
+    }
+    
 }
